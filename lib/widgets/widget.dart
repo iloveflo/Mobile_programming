060266b4715bca9355908f378882a2b0476f8e29
ThangG <nguyenmanhthang2k5@gmail.com>
2026-09-09 07:29:50 +0700

    fix: format widget file

--- lib/widgets/widget.dart
+++ lib/widgets/widget.dart
@@ -1,4 +1,4 @@
-// thư mục widget để viết những thành phần dùng chung tái sử dụng dc nhiều lần, đóng vai trò là khung cho Frontend như: 
+// thư mục widget để viết những thành phần dùng chung tái sử dụng dc nhiều lần, đóng vai trò là khung cho Frontend như:
 // nút bấm
 // phông chữ
 // định dạng
